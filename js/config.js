3bdf2ab607e73d0ecd78698921b62410baa28228
Slim7064 <samsunghwanlim@gmail.com>
2024-08-21 12:26:44 -0700

    final

--- js/config.js
+++ js/config.js
@@ -1,10 +1,13 @@
 window._config = {
     cognito: {
+        // Get from Cognito
         userPoolId: 'us-east-1_OtieyQej4', // e.g. us-east-2_uXboG5pAb
+        // Get from app client ID
         userPoolClientId: '77us1adlp7s3jk2ng3eg1a1u2q', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
-        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        // Get from API Gateway URL
+        invokeUrl: 'https://lw1kolw4r3.execute-api.us-east-1.amazonaws.com/dev' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
